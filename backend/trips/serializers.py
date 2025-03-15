015767cbf59e6c31ccbecfedfffb2fdd127589b1
MwirigiMuriithi <learnerviner@gmail.com>
2025-03-15 18:25:22 +0300

    Updated backend/trips/serializers.py

--- backend/trips/serializers.py
+++ backend/trips/serializers.py
@@ -2,6 +2,20 @@ from rest_framework import serializers
 from .models import Trip
 
 class TripSerializer(serializers.ModelSerializer):
+    eldFormData = serializers.JSONField(read_only=True)
+    
     class Meta:
         model = Trip
-        fields = '__all__'
+        fields = [
+            'id',
+            'current_location',
+            'pickup_location',
+            'dropoff_location',
+            'current_cycle_hours_used',
+            'route',
+            'logs',
+            'distance',
+            'fuel_stops',
+            'eldFormData'
+        ]
+

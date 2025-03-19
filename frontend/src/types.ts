ccfcd6fd8997830e4547f6f320c691af31f3e8e7
MwirigiMuriithi <learnerviner@gmail.com>
2025-03-19 10:14:30 +0300

    Updated TypeScript types for frontend

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,29 +1,35 @@
 // src/types.ts
 export interface Event {
-    status: string;
-    start: string;
-    end: string;
-    description: string;
-  }
-  
-  export interface DayLog {
-    dayIndex: number;
-    events: Event[];
-  }
-  
-  export interface FuelStop {
-    mile: number;
-    location: string;
-  }
-  
-  export interface TripData {
-    current_location: string;
-    pickup_location: string;
-    dropoff_location: string;
-    current_cycle_hours_used: number;
-    distance: number;
-    route: [number, number][];
-    logs: DayLog[];
-    fuel_stops: FuelStop[];
-  }
-  
+  status: string;
+  start: string;
+  end: string;
+  description: string;
+}
+
+export interface DayLog {
+  dayIndex: number;
+  events: Event[];
+}
+
+export interface FuelStop {
+  mile: number;
+  location: string;
+}
+
+export interface EldFormData {
+  dayIndex: number;
+  timeline: string[];
+}
+
+export interface TripData {
+  driver: number;
+  current_location: string;
+  pickup_location: string;
+  dropoff_location: string;
+  cycle_hours_used: number;
+  distance: number;
+  route: [number, number][];
+  logs: DayLog[];
+  fuel_stops: FuelStop[];
+  eldFormData: EldFormData[];
+}
